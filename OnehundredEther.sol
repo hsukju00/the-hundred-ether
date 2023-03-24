3acf6fac3156f9f03bcf6f67fbe73bc877f795ca
HongSukJu <ehensnfl@kookmin.ac.kr>
2023-03-24 17:03:42 +0900

    OnehundredEther.sol 작성

---
+++ OnehundredEther.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: NONE
+
+pragma solidity >= 0.7.0;
+
+contract OnehundredEther {
+    
+    struct Area {
+        bool isPurchased;
+        address owner;
+        string url;
+    }
+
+    address public owner;
+    uint public numPurchasedArea;
+    Area[10000] private areas;
+
+    constructor() {
+        owner = msg.sender;
+        numPurchasedArea = 0;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function purchaseArea(uint _index, string memory _url) public payable {
+        require(!areas[_index].isPurchased);
+        require(msg.value == 0.01 ether);
+
+        Area storage area = areas[_index];
+        area.owner = msg.sender;
+        area.url = _url;
+        numPurchasedArea++;
+    }
+
+    function replaceAreaUrl(uint _index, string memory _url) public payable {
+        require(areas[_index].owner == msg.sender);
+
+        Area storage area = areas[_index];
+        area.url = _url;
+    }
+
+    function withdrawFunds() public onlyOwner {
+        if (!payable(owner).send(address(this).balance))
+            revert();
+    }
+    
+}
